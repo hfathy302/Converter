a4c9cdf0013e760a5d0cedcb1c86f2a9813ffce1
Hany Mubarak <30952470+hfathy302@users.noreply.github.com>
2017-08-18 01:11:58 +0200

    Add files via upload
    Files

---
+++ cal.js
@@ -0,0 +1,7 @@
+
+
+
+/* When the input field receives input, convert the value from pounds to kilograms */
+function priceConverter(valNum) {
+    document.getElementById("outputEPG").innerHTML = valNum * 17.95;
+}
